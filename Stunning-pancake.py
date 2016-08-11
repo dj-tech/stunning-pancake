a92a6536709caa9579848a1bb181582de54bceae
dj-tech <www.giuseppe@dellavedova.org>
2016-08-11 20:22:53 +0200

    2.0 version

--- Stunning-pancake.py
+++ Stunning-pancake.py
@@ -1,2 +1,33 @@
 # -*- coding: encoding -*-
-Emoticon = (😺)
+Emoticon = ("😺")
+for i in range (10,1001):
+   break
+import os
+os.system ("curl")
+numeri = ("".join(i,str(7ⁿ-2/4)))
+if numeri == range (999,1001):
+   print ("😀")
+else:
+   print ("😊")
+import json
+Save = ["numeri": numeri]
+print (json.dumps (Save))
+f = open('workfile', 'w')
+f.write('This is a test\n')
+import socket
+socket.getaddrinfo("stunning-pancake", 80, proto=socket.IPPROTO_TCP)[(<AddressFamily.AF_INET6: 10>, <SocketType.SOCK_STREAM: 1>,6, '', ('2606:2800:220:1:248:1893:25c8:1946', 80, 0, 0)),(<AddressFamily.AF_INET: 2>, <SocketType.SOCK_STREAM: 1>,6, '', ('93.184.216.34', 80))]
+
+HOST = 'stunning-pancake'                 # Symbolic name meaning all available interfaces
+PORT = 80              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
+import random
+random.choice(['apple', 'pear', 'banana'])
